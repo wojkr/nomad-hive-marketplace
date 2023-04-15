e73b6dd919068a97676bb1a6f0b5d4800be3a77b
wojciech krupa <wojciech.w.krupa@gmail.com>
2023-04-15 21:33:00 +0100

    listings: prisma next req setup

---
+++ app/api/listings/route.ts
@@ -0,0 +1,38 @@
+import prisma from "@/app/libs/prismadb";
+import getCurrentUser from "@/app/actions/getCurrentUser";
+import { NextResponse } from "next/server";
+
+export async function POST(request: Request) {
+  const currentUser = await getCurrentUser();
+  if (!currentUser) return NextResponse.error();
+
+  const body = await request.json();
+
+  const {
+    category,
+    location,
+    guestCount,
+    roomCount,
+    bathroomCount,
+    imageSrc,
+    price,
+    title,
+    description,
+  } = body;
+
+  const listing = await prisma.listing.create({
+    data: {
+      category,
+      locationValue: location.value,
+      guestCount,
+      roomCount,
+      bathroomCount,
+      imageSrc,
+      price: parseInt(price, 10),
+      title,
+      description,
+      userId: currentUser.id,
+    },
+  });
+  return NextResponse.json(listing);
+}
