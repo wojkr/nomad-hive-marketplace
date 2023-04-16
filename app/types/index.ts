38703846e7816f1ed0c90f3b755f2dc439d420ac
wojciech krupa <wojciech.w.krupa@gmail.com>
2023-04-16 19:44:07 +0100

    added safe types: listing and reservation

--- app/types/index.ts
+++ app/types/index.ts
@@ -1,4 +1,4 @@
-import { User } from "@prisma/client";
+import { User, Listing, Reservation } from "@prisma/client";
 
 export type SafeUser = Omit<
   User,
@@ -7,4 +7,18 @@ export type SafeUser = Omit<
   createdAt: string;
   updatedAt: string;
   emailVerified: string | null;
+};
+export type SafeListing = Omit<
+  Listing,
+  "createdAt" | "updatedAt" | "emailVerified"
+> & {
+  createdAt: string;
+};
+export type SafeReservation = Omit<
+  Reservation,
+  "createdAt" | "updatedAt" | "emailVerified"
+> & {
+  createdAt: string;
+  startDate: string;
+  endDate: string;
 };
