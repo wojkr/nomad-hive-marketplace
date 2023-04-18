5ed37e6d3c48b00ea2cb2433124236b2766ce9e4
wojciech krupa <wojciech.w.krupa@gmail.com>
2023-04-18 23:41:09 +0100

    styles

--- app/listings/[listingId]/ListingClient.tsx
+++ app/listings/[listingId]/ListingClient.tsx
@@ -1,9 +1,12 @@
 "use client";
-import React from "react";
+import React, { useMemo } from "react";
 import { SafeUser, SafeListing } from "@/app/types/index";
 import { Reservation } from "@prisma/client";
+import { categories } from "@/app/components/navbar/Categories";
+import ListingHead from "@/app/components/listings/ListingHead";
+import ListingInfo from "@/app/components/listings/ListingInfo";
 
-interface IListingClient {
+interface ListingClientProps {
   currentUser?: SafeUser | null;
   listing: SafeListing & {
     user: SafeUser;
@@ -11,8 +14,45 @@ interface IListingClient {
   reservation?: Reservation[];
 }
 
-const ListingClient: React.FC<IListingClient> = ({ currentUser, listing }) => {
-  return <div>ListingClient</div>;
+const ListingClient: React.FC<ListingClientProps> = ({
+  currentUser,
+  listing,
+}) => {
+  const category = useMemo(() => {
+    return categories.find((c) => c.label === listing.category);
+  }, [listing.category]);
+  return (
+    <div className="max-w-screen-lg mx-auto">
+      <div className="flex flex-col gap-6">
+        <ListingHead
+          title={listing.title}
+          imageSrc={listing.imageSrc}
+          locationValue={listing.locationValue}
+          id={listing.id}
+          currentUser={currentUser}
+        />
+        <div
+          className="
+              grid 
+              grid-cols-1 
+              md:grid-cols-7 
+              md:gap-10 
+              mt-6
+            "
+        >
+          <ListingInfo
+            user={listing.user}
+            category={category}
+            guestCount={listing.guestCount}
+            roomCount={listing.roomCount}
+            bathroomCount={listing.bathroomCount}
+            description={listing.description}
+            locationValue={listing.locationValue}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ListingClient;
--- app/listings/[listingId]/page.tsx
+++ app/listings/[listingId]/page.tsx
@@ -23,7 +23,6 @@ const ListingPage = async ({ params }: { params: IParams }) => {
   return (
     <ClientOnly>
       <ListingClient listing={listing} currentUser={currentUser} />
-      <div>{listing.title}</div>;
     </ClientOnly>
   );
 };
