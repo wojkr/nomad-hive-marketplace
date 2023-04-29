b59858bde9b4d5b1a117e5db3e92a51ea6139846
wojciech krupa <wojciech.w.krupa@gmail.com>
2023-04-29 22:59:08 +0100

    added error page

---
+++ app/error.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import React, { useEffect } from "react";
+import EmptyState from "./components/EmptyState";
+
+interface ErrorProps {
+  error: Error;
+}
+
+const error: React.FC<ErrorProps> = ({ error }) => {
+  useEffect(() => {
+    console.error(error);
+  }, [error]);
+
+  return <EmptyState title="Uh Oh" subtitle="Something went wrong..." />;
+};
+
+export default error;
